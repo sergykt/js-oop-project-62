3cff618c2d5972ee6f82d82a2760f413cc013e75
Sergei Sivtsev <sergykt1992@mail.ru>
2023-05-02 12:38:07 +0600

    step 3

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ local-install:
 	npm install
 	sudo npm link
 
+uninstall:
+	sudo npm uninstall -g @hexlet/code
+
 publish:
 	npm publish --dry-run
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,71 @@
-import Validator from './validator.js';
+import * as yup from 'yup';
 
-const makeValidator = () => new Validator();
+class Validator {
+  constructor() {
+    this.schema = yup;
+  }
 
-export default makeValidator;
+  string() {
+    const newSchema = new Validator();
+    newSchema.schema = this.schema.string().notRequired();
+    newSchema.type = 'string';
+    return newSchema;
+  }
+
+  required() {
+    this.schema = this.schema.required();
+    return this;
+  }
+
+  minLength(length) {
+    this.schema = this.schema.min(length);
+    return this;
+  }
+
+  contains(value) {
+    this.schema = this.schema.matches(value, `The string doesn't contain ${value}`);
+    return this;
+  }
+
+  number() {
+    const newSchema = new Validator();
+    newSchema.schema = this.schema.number().notRequired();
+    newSchema.type = 'number';
+    return newSchema;
+  }
+
+  positive() {
+    this.schema = this.schema.positive();
+    return this;
+  }
+
+  range(from, to) {
+    this.schema = this.schema.min(from).max(to);
+    return this;
+  }
+
+  array() {
+    const newSchema = new Validator();
+    newSchema.schema = this.schema.array().of(yup.mixed()).notRequired();
+    newSchema.type = 'number';
+    return newSchema;
+  }
+
+  sizeof(length) {
+    this.schema = this.schema.length(length);
+    return this;
+  }
+
+  object() {
+    const newSchema = new Validator();
+    newSchema.schema = this.schema.object();
+    newSchema.type = 'object';
+    return newSchema;
+  }
+
+  isValid(value) {
+    return this.schema.isValidSync(value);
+  }
+}
+
+export default Validator;
